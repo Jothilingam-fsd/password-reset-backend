f357ef81d0fcde15c3068ee03279b8733e4eadbb
Jothilingam-fsd <jothilingamfsd@gmail.com>
2025-06-28 16:04:05 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { forgotPassword, verifyToken, resetPassword } from '../controllers/authController.js';
+import {
+  signup,
+  forgotPassword,
+  verifyToken,
+  resetPassword
+} from '../controllers/authController.js';
 
 const router = express.Router();
 
+router.post('/signup', signup); 
 router.post('/forgot-password', forgotPassword);
-router.get('/reset-password/:token', verifyToken);
-router.post('/reset-password/:token', resetPassword);
+router.get('/reset-password/:userId/:token', verifyToken);
+router.post('/reset-password/:userId/:token', resetPassword);
 
 export default router;
