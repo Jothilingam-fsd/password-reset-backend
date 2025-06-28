a00bfd4e11d5cdc4aad0153fe292c2fa576e1702
Jothilingam-fsd <jothilingamfsd@gmail.com>
2025-06-28 16:04:45 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,15 +1,21 @@
+// utils/sendEmail.js
 import nodemailer from 'nodemailer';
 
 const sendEmail = async (to, subject, html) => {
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    service: 'gmail',
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
+      user: process.env.EMAIL_USER,      // from .env file
+      pass: process.env.EMAIL_PASS       // Gmail app password
     }
   });
 
-  await transporter.sendMail({ from: process.env.EMAIL_USER, to, subject, html});
+  await transporter.sendMail({
+    from: `"MyApp" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    html
+  });
 };
 
 export default sendEmail;
